a146ab614c11ebc0bb2c274f7930a6a035166a89
maxim-polyakov <maxim7012@mail.ru>
2022-06-15 20:37:56 +0300

    sql answer module has beed added

--- NLP/Answers.py
+++ NLP/Answers.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import random
+import NLP
 from NLP import mapa
-
+from sqlalchemy import create_engine
 
 class Answer:
 
@@ -13,14 +14,22 @@ class Answer:
 
 
 class RandomAnswer(Answer):
+    
+    conn = NLP.psycopg2.connect("dbname=postgres user=postgres password=postgres")
 
-    data = pd.read_excel('./datasets/dataset.xlsx')
+    inpt = pd.read_sql('SELECT * FROM hiset', conn)
+
+    data = NLP.pd.concat([inpt])
     df = []
+
 
     def __init__(self):
         pass
 
     def answer(self):
+        
+
+        
         outmapa = []
         for i in range(0, len(self.data['text'])-1):
             if(self.data['hi'][i] == 1):
